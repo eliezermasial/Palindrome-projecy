ad8e4805058daeb8c847ad8d69f7c68b37d84078
unknown <eliezermasiala2000@gmail.com>
2023-06-21 14:25:32 +0200

    modification

--- palindrom.js
+++ palindrom.js
@@ -1,31 +1,33 @@
 
 function palindrome(){
+
     const input= document.querySelector('#input')
     const button= document.querySelector('#button')
-    const span= document.querySelector('#reponse')
-
+    const span= document.querySelector('#answer')
 
     const inputvalue= input.value
     const reverse= inputvalue.split('').reverse('').join('')
     
- 
     button.addEventListener('click',(e)=>{
-        e.preventDefault()
-        if(inputvalue == reverse){
-            
-            span.innerHTML= `yes  ${inputvalue} is palindrome`
-            span.style.textDecoration = "underline"
-            span.style.color = 'blue'
-            
-        } else if(inputvalue !== reverse){
 
-            span.innerHTML= `no, ${inputvalue} is not palindrome`
-            span.style.textDecoration = "underline"
-            span.style.color = 'red'
-        }else{
-            span.innerHTML= "impossible"
-        }
+        e.preventDefault()
+        e.stopPropagation()
         
+        if(inputvalue !== reverse){
+            
+            console.log(inputvalue.length)
+            span.innerHTML= `no, ${inputvalue} is not palindrome`
+            span.style.color = 'var(--bg-body)'
+            
+        } else{
+            span.innerHTML= `yes  ${inputvalue} is palindrome`
+            span.style.color = 'var(--bg-body)'
+        }
+        if(inputvalue.length < 1){
+
+            span.innerHTML= `unable to perform palindrome operation.`
+            span.style.color = 'var(--bg-body)'
+        }
     })
     
 }
